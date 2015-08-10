9296cd1bccdff1d0caff3f622dc4de109ce0e6b2
Tyler <tylerhines1992@gmail.com>
2015-08-10 14:57:31 -0400

    added resize through clicked function

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,31 +1,24 @@
 
 var userChoice = 16;
 
-
-function grid() {
-
-  $(document).ready(function(){
-    for(i = 0; i < userChoice; i++) {
-      $("body").append("<div class='container'></div>");
-    }
-  });
-
-  $(document).ready(function() {
-    for(i = 0; i < userChoice; i++) {
-      $(".container").append("<div></div>");
-    };
-  })
-  $(document).ready(function() {
-    $(".container div").mouseenter(function() {
-      $(this).addClass("highlight");
-      });
-    });
-}
-
-grid();
-
 function clicked() {
      $(".container").remove();
      userChoice = prompt("How many squares on each side?");
-     grid()
+     var square_width = $('.drawpad').width()/userChoice - 2;
+
+     if (userChoice >=1 && userChoice <= 128) {
+       for(i = 0; i < userChoice; i++) {
+         $(".drawpad").append("<div class='container'></div>");
+       }
+       for(i = 0; i < userChoice; i++) {
+         $(".container").append("<div></div>");
+       };
+       $('.container div').css('width',square_size);
+		   $('.container div').css('height',square_size);
+
+       $(".container div").mouseenter(function() {
+         $(this).addClass("highlight");
+         });
+
+     }
 };
